284561ff0c5912befb43512c2b0ad89f0249e42c
WenXin <3074463749@qq.com>
2025-02-07 16:39:25 +0800

    修改main.py的import逻辑

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from utils.visualization import plot_and_save_csv
-from utils.dataset_construction import format_folders
-from data_processing import data_processing
+from utils.visualization import *
+from utils.dataset_construction import *
+from data_processing import *
 
 # 建立数据集
 data_path = r"C:\Users\30744\Desktop\手互\数据集2"
 dataset_path = r"C:\Users\30744\Desktop\手互\数据集2.1"
 pdf_path = r"C:\Users\30744\Desktop\手互\手语采样数据集.pdf"
-# pdf_construct_dataset(pdf_path, dataset_path)
+pdf_construct_dataset(pdf_path, dataset_path)
 
 # 格式化数据集中的数据
 # format_folders(dataset_path, data_path)
