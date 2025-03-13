5bbd7ec1b2847b2d5ab6158e1da14c0093087827
0yyyyyy0 <hsy3157772814@outlook.com>
2025-03-13 15:07:10 +0800

    将dataset数据滑窗生成可以使用的变量

--- utils/slipping_window.py
+++ utils/slipping_window.py
@@ -36,11 +36,11 @@ def get_feature_window(subject_num):
             file = os.path.join(dataset_path,traget_dataset,category, file_name)
             df = pd.read_csv(file)  # 读取信号
             x = df.iloc[:, 0:n_channels].values.astype(np.float32)  # (,)
-            x_repeat = np.tile(x[:, np.newaxis], (1, 4))  # 形状变为 (86329, 4)
+            x_repeat = np.tile(x[:, np.newaxis], (1, 4))  # 将第一列复制四份变成 (86329, 4)这里重要
 
             for l_use in range(len(x)):
-                if (l_use % 1 == 0) & (l_use >= 128):  # 间隔80ms进行切片
-                    data_win = x_repeat[l_use - 128:l_use]  # 对信号切片取时间窗320ms
+                if (l_use % 1 == 0) & (l_use >= 128):  
+                    data_win = x_repeat[l_use - 128:l_use]  
                     data_all[idx].append(data_win)
                     label_all[idx].append(idx)
 
@@ -52,39 +52,39 @@ def get_feature_window(subject_num):
     for c in range(classes):
         data_all[c] = np.asarray(data_all[c])  # 转为numpy
         data_all[c] = data_all[c].astype('float16')  # 转为float16
-        data_all[c] = np.array(data_all[c]).reshape((-1, 1, 128, 4))  # 调整形状(分类数12,窗口数，1，窗口长64，通道数1)
+        data_all[c] = np.array(data_all[c]).reshape((-1, 1, 128, 4))  # 调整形状(分类数12,窗口数，1，窗口长128，通道数1)这里重要
         label_all[c] = np.asarray(label_all[c])
         label_all[c] = label_all[c].astype('int8')
     return data_all,label_all,data_split_point,datanum_all
 
 subject_num=1
-def result_intra(x, y,datanum_all):#(用户数27，分类数16，窗口数，1，窗口长32，特征数10)
+def result_intra(x, y,datanum_all):
     x1_a_subject=[]
     x1_b_subject = []
     y1_a_subject = []
     y1_b_subject = []
     for subject in range(subject_num):
         #print('s'+str(subject))
-        X1 = x[subject]  # Train 某一用户的所有数据(分类数16，窗口数，1，窗口长32，特征数10)
+        X1 = x[subject]  # Train 某一用户的所有数据(分类数，窗口数，1，窗口长128，通道数4)
 
         X1_a, X1_b, Y1_a, Y1_b = [], [], [], []
         #data_split_point_for_train = []
         for c in range(classes):
             ges_num=datanum_all[subject][c]
-            X1_a.append(X1[c][:int(ges_num*0.6)])#对第c个手势切成两片#(16,224,1,32,10)
+            X1_a.append(X1[c][:int(ges_num*0.6)])#前60%作为训练集，后40%作为测试集
             X1_b.append(X1[c][int(ges_num*0.6):])
             Y1_a.append([c]*len((X1[c][:int(ges_num*0.6)])))
             Y1_b.append([c]*len((X1[c][int(ges_num*0.6):])))
 
-        X1_a = np.vstack(X1_a)#(3085,1,32,10)
+        X1_a = np.vstack(X1_a)#所有类别合并
         X1_b = np.vstack(X1_b)
         Y1_a = np.hstack(Y1_a)
         Y1_b = np.hstack(Y1_b)
-        x1_a_subject.append(X1_a)#(27，3584,1,32,10)训练
-        x1_b_subject.append(X1_b)#测试
+        x1_a_subject.append(X1_a)
+        x1_b_subject.append(X1_b)
         y1_a_subject.append(Y1_a)#
         y1_b_subject.append(Y1_b)
-    return x1_a_subject,y1_a_subject,x1_b_subject,y1_b_subject#(用户数，窗口数，1，窗口长64，通道数1)
+    return x1_a_subject,y1_a_subject,x1_b_subject,y1_b_subject#(用户数，窗口数，1，窗口长64，通道数4)
 '''
 输入文件，
 '''
